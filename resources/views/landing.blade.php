70c963d70a07ffa2830f3c80f3a2c9db13c5e2e9
Bunhann Thou <bunhann@gmail.com>
2019-09-06 15:49:11 +0700

    landing page.

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{env('APP_NAME')}}</title>
+    <link rel="stylesheet" href="{!! asset('css/landing.css') !!}" />
+</head>
+<body>
+<div class="full-screen">
+  <div>
+    <h1>{{env('APP_NAME')}}</h1>
+    <br>
+    <a class="button-line" href="https://open.org.kh">Info</a> 
+  </div>
+</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,14 @@
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
 });
+
 
 Auth::routes();
 
