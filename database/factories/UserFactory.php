78dd64d0860ee674a6d4c4fcef1b5796924efee6
Socheat <socheatsok78@gmail.com>
2019-09-18 10:53:12 +0700

    Add api_token column to users table
    - Update config/auth.php, enable token hash
    - Update UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,11 +17,14 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $token = Str::random(60);
+
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'api_token' => hash('sha256', $token),
         'remember_token' => Str::random(10),
     ];
 });
